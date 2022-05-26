f6aa2b460f0a086b3eda0336b828513d0d80827f
Berkay Saraç <berkay.sarac420@gmail.com>
2022-05-26 23:10:12 +0300

    routes added

--- express-router/admins/app.js
+++ express-router/admins/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.router();
 
-router.post("/products",(req,res,next)=>{
-  res.send("you are in the products");
+router.post("/products", (req, res, next) => {
+    res.send("you are in the products on the admins");
 })
 
-router.get("/add-product",(req,res,next)=>{
-  res.send("you are in the add product")
+router.get("/add-product", (req, res, next) => {
+    res.send("you are in the add product on the admins")
 })
 
 module.exports = router;
