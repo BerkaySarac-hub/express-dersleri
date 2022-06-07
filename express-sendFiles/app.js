e3daa5cd3d4953d183bc3e152ce53dbfeb4cf1f6
Berkay Saraç <40615491+BerkaySarac-hub@users.noreply.github.com>
2022-06-08 00:31:30 +0300

    express ap

--- express-sendFiles/app.js
+++ express-sendFiles/app.js
@@ -7,14 +7,13 @@ app.use("/main-page", (req, res, next) => {
     console.log("Kullanıcı ana sayfada kullanıcı bilgisi", req.ip);
 })
 
-
-
-
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "/errorPage.html"))
+})
 
 app.listen(3000, "127.0.0.1", (error) => {
     if (error) {
         console.log("Bir hata meydana geldi", error);
-
     }
     console.log("server working on 3000 port server : 127.0.0.1");
 })
